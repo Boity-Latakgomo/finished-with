70e6e53afe459521028169ab06233e7cd2b04185
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-11-17 20:40:31 +0200

    encapsulating styles

--- App.js
+++ App.js
@@ -3,16 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
+import AppText from './app/components/AppText';
+
 export default function App() {
   return (
     <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
-    <View style={{
-      backgroundColor: "dodgerblue",
-      width: 100,
-      height: 100,
     
-    }}
-    ></View>
+    <AppText>I love React Native!</AppText>
+    
     </View>
-  )
+    
+  );
 }
