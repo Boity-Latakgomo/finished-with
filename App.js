74a319a54d710c6b990315741d13acdecb174ce6
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-11-19 14:59:38 +0200

    login button

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
-import AppText from './app/components/AppText';
+//import AppText from './app/components/AppText/AppText';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
+  console.log("in app js")
   return (
     <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
     
-    <AppText>I love React Native!</AppText>
+    <AppButton title="Login" />
     
     </View>
     
